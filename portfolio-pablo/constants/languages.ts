6cd50cfb62a1c53af4553e2b876c695b863e7f98
Pablo Vallejo <admin@dfbubbles.com>
2026-05-15 16:46:26 +0700

    feat: language data

---
+++ portfolio-pablo/constants/languages.ts
@@ -0,0 +1,12 @@
+import type { Language } from "@/types";
+
+export const languages: Language[] = [
+  { language: "Spanish", fluency: "Native", level: "native" },
+  { language: "Catalan", fluency: "Native", level: "native" },
+  {
+    language: "English",
+    fluency: "Full Professional Proficiency",
+    level: "fluent",
+  },
+  { language: "Thai", fluency: "Beginner", level: "beginner" },
+];
